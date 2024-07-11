fb0b519d38c0de33281c92ddc94dea9c4e96ec32
yasser kaci <kaciyasserabdeldjalil@gmail.com>
2024-07-11 20:46:42 +0100

    first commit

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import bodyParser from "body-parser";
+
+//connect express app
+const app = express();
+
+// connect mongoDB
+
+//middlewares
+
+//SCHEMA
+
+//ROUTES
+
+app.listen(8000, () => console.log("app runing at port 8000"));
